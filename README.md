1efe4e27501619ff50e7f87eb48259e1a7f67cff
Fran Guijarro <franleplant@gmail.com>
2020-07-12 12:29:42 -0300

    add packages

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python Modules
+
+A simple demonstration of how python modules work
+
+Read more here: https://docs.python.org/3/tutorial/modules.html
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import siblingMod
 import childMod.main
+import childPackage.main
 
 print("im main py")
 
 siblingMod.sayHi()
 childMod.main.sayHi()
+childPackage.main.sayHi()
